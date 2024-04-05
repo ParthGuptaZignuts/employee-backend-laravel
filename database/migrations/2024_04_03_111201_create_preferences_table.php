d446903847f1481e90e2e935ba42383016a2af84
Zignuts Parth Gupta <parthg@zignuts.com>
2024-04-05 11:03:39 +0530

    unable to solve prefernce table problem

--- database/migrations/2024_04_03_111201_create_preferences_table.php
+++ database/migrations/2024_04_03_111201_create_preferences_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('preferences', function (Blueprint $table) {
             $table->increments('code');
             $table->string('value', 64);
-            $table->timestamps();
         });
     }
 
