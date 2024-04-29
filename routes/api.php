dcd8829e51c6ac89735dfaf135d92c08c9d03d0a
Zignuts Parth Gupta <parthg@zignuts.com>
2024-04-29 12:29:59 +0530

    added the comments in api

--- routes/api.php
+++ routes/api.php
@@ -18,22 +18,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//  public routes
+// public routes
+
+// route for registering
 Route::post('/register', [AuthenticationController::class, 'createUser']);
+// route for login
 Route::post('/login', [AuthenticationController::class, 'loginUser']);
+// route for password reset
 Route::post('/password/reset', [AuthenticationController::class, 'resetPassword']);
+// route for getting the company detials 
 Route::get('/companyinfo', [CompanyController::class, 'companyWithLogo']);
+// route for getting all jobs detials
 Route::get("/jobsInfo", [JobDescriptionController::class, "AllJobsInfo"]);
+// route for getting jobs Status
 Route::get("/jobsStatus", [JobApplicationController::class, "JobsStatus"]);
 
 
-// user registration protected routes
+// protected routes
 Route::middleware('auth:sanctum')->group(function () {
     // routes for logout , get SA and CA statistics and get user
     Route::post('/logout', [AuthenticationController::class, 'logout']);
     Route::get('/user', [AuthenticationController::class, 'getUser']);
     Route::get('/statistics', [StatisticsController::class, 'getStatistics']);
 
+    // route with prefix of companies and crud (CREATE READ UPDATE DELETE) of companies
     Route::middleware('checkUserType:SA')->prefix('/companies')->group(function () {
         Route::post('/create', [CompanyController::class, 'store']);
         Route::post('/{id}', [CompanyController::class, 'update']);
@@ -42,7 +50,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/{id}', [CompanyController::class, 'show']);
     });
 
-    // job routes for CRUD operations
+    // job routes for CRUD (CREATE READ UPDATE DELETE) operations
     Route::middleware('checkUserType:SA,CA')->group(function () {
         Route::get('jobs', [JobDescriptionController::class, 'index']);
         Route::prefix('job')->group(function () {
@@ -53,7 +61,7 @@ Route::middleware('auth:sanctum')->group(function () {
         });
     });
 
-    // employee routes for CRUD operations
+    // employee routes for CRUD (CREATE READ UPDATE DELETE) operations
     Route::middleware('checkUserType:SA,CA')->group(function () {
         Route::get('employees', [EmployeeController::class, 'index']);
         Route::get('getallcompanies', [CompanyController::class, 'getAllCompanies']);
@@ -65,15 +73,14 @@ Route::middleware('auth:sanctum')->group(function () {
         });
     });
 
-    // Job application routes for CRUD operations 
+    // Job application routes for CRUD (CREATE READ UPDATE DELETE) operations 
     Route::middleware('checkUserType:SA,CA')->group(function () {
         Route::prefix('allCandidateInfo')->group(function () {
             Route::get('', [JobApplicationController::class, 'getAllDetails']);
             Route::get('/{id}', [JobApplicationController::class, 'show']);
-            Route::post('/update/{id}', [JobApplicationController::class,'update']);
-            Route::post('/delete/{id}', [JobApplicationController::class,'delete']);
+            Route::post('/update/{id}', [JobApplicationController::class, 'update']);
+            Route::post('/delete/{id}', [JobApplicationController::class, 'delete']);
         });
-
     });
 
     Route::post("/userJobDetails", [JobApplicationController::class, 'store']);
