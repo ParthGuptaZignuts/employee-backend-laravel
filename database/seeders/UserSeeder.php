7494b73fab90e9166add7f1357884e209758e779
Zignuts Parth Gupta <parthg@zignuts.com>
2024-04-29 17:57:03 +0530

    corrected the way of generating emp_number in userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,11 +6,13 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use App\Models\Preference;
 
 class UserSeeder extends Seeder
-{
+{   
     public function run(): void
     {
+        $employeeNumber = $this->generateEmployeeNumber();
         // Company Admin
         User::create([
             'first_name' => 'Company1',
@@ -23,9 +25,9 @@ class UserSeeder extends Seeder
             'type' => 'CA',
             'company_id' => 1,
             'joining_date' => now(),
-            'employee_number' => "1",
+            'employee_number' => $employeeNumber,
         ]);
-
+        $nextEmployeeNumber = $this->generateEmployeeNumber();
         // Employee
         User::create([
             'first_name' => 'Employee1',
@@ -38,8 +40,33 @@ class UserSeeder extends Seeder
             'type' => 'E',
             'company_id' => 1,
             'joining_date' => now(),
-            'employee_number' => "2",
+            'employee_number' => $nextEmployeeNumber,
         ]);
 
     }
+
+    public function generateEmployeeNumber(): string
+    {
+        try {
+            $latestEmployeeNumberPref = Preference::where('code', 'EMP')->first();
+
+            if ($latestEmployeeNumberPref) {
+                $latestEmployeeNumber = (int)$latestEmployeeNumberPref->value;
+                $nextEmployeeNumber = 'EMP' . str_pad($latestEmployeeNumber + 1, 5, '0', STR_PAD_LEFT);
+                $latestEmployeeNumberPref->value = $latestEmployeeNumber + 1;
+                $latestEmployeeNumberPref->save();
+            } else {
+                $nextEmployeeNumber = 'EMP00001';
+                $latestEmployeeNumberPref = new Preference();
+                $latestEmployeeNumberPref->code = 'EMP';
+                $latestEmployeeNumberPref->value = 1;
+                $latestEmployeeNumberPref->save();
+            }
+
+            return $nextEmployeeNumber;
+        } catch (\Exception $e) {
+            throw new \Exception("An unexpected error occurred while generating employee number.");
+        }
+    }
+
 }
